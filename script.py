43324c697ecad909138b2edaae4a347945d720aa
Молчанов Данил Андреевич <damolchanov@edu.hse.ru>
2022-03-20 12:59:27 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 my_date = datetime.now()
-f = open ('/var/www/html/students/damolchanov.html','w')
+f = open ('/var/www/html/students/damolchanov/damolchanov.html','w')
 f.write(my_date.isoformat())
 f.close()
