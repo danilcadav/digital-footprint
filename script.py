426baeba362b943943a73d099b96b11f10852843
Молчанов Данил Андреевич <damolchanov@edu.hse.ru>
2022-03-24 21:01:54 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
+import numpy as np
+from jinja2 import Template
+import plotly.graph_objects as go
 from datetime import datetime
+
+temp = open('template.html').read()
+template = Template(temp)
+
+
 my_date = datetime.now()
-f = open ('/var/www/html/students/damolchanov/damolchanov.html','w')
-f.write(my_date.isoformat())
-f.close()
+
+x = np.arange(0, 5, 0.1)
+def f(x):
+    return x**2
+
+fig = go.Figure([go.Scatter(x=x, y=f(x))])
+
+with open('index.html', 'w') as fh:
+    fh.write(template.render(fullname = "Молчанов Данил Андреевич",datetime = my_date.isoformat(), group = "БИВ 213", git = fig.to_html(), tai = fig.to_html(), jit = fig.to_html(), zul = fig.to_html()))
